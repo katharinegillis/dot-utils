f7532c3d47e415788638d4364c6c9b5c63181482
Katharine Cordes <gillis.katharine@gmail.com>
2021-07-18 15:08:45 -0600

    chore: fix typo

--- update.sh
+++ update.sh
@@ -6,5 +6,5 @@ if [ "$SYSTEM" != "mac" ]; then
     # Install htpasswd utility
     sudo apt-get install -y apache2-utils
 else
-    echo -e "\e[33m No utilities to install for MacOS."
+    echo -e "\e[33m No utilities to update for MacOS."
 fi
