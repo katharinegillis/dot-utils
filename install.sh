bd5c1d342ba05366c54ddf082929682208ca4677
Katharine Cordes <gillis.katharine@gmail.com>
2021-07-18 15:00:57 -0600

    chore: add info for why nothing installs on mac

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ SYSTEM=$3
 if [ "$SYSTEM" != "mac" ]; then
     # Install htpasswd utility
     sudo apt-get install -y apache2-utils
+else
+    echo -e "\e[33m No utilities to install for MacOS."
 fi
--- uninstall.sh
+++ uninstall.sh
@@ -5,4 +5,6 @@ SYSTEM=$3
 if [ "$SYSTEM" != "mac" ]; then
     # Uninstall htpasswd utility
     sudo apt-get remove -y apache2-utils
+else
+    echo -e "\e[33m No utilities to uninstall for MacOS."
 fi
