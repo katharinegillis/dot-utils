b3286e366844610baee9864dcd6489787e97948b
Katharine Cordes <gillis.katharine@gmail.com>
2021-07-18 14:27:21 -0600

    feat: add mac commands

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-# Uninstall htpasswd utility
-sudo apt-get remove -y apache2-utils
+SYSTEM=$3
+
+if [ "$SYSTEM" != "mac" ]; then
+    # Uninstall htpasswd utility
+    sudo apt-get remove -y apache2-utils
+fi
--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-# Install htpasswd utility
-sudo apt-get install -y apache2-utils
+SYSTEM=$3
+
+if [ "$SYSTEM" != "mac" ]; then
+    # Install htpasswd utility
+    sudo apt-get install -y apache2-utils
+fi
