79e2ae109b746dd5e49704f4a17af91a83017df2
Katharine Cordes <gillis.katharine@gmail.com>
2022-08-13 23:20:26 -0600

    refactor: remove non-Linux logic from package

--- install.sh
+++ install.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-SYSTEM=$3
-
-if [ "$SYSTEM" != "mac" ]; then
-    # Install htpasswd utility
-    sudo apt-get install -y apache2-utils
-else
-    echo -e "\e[33m No utilities to install for MacOS."
-fi
+# Install htpasswd utility
+sudo apt-get install -y apache2-utils
